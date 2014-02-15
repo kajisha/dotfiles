45263e93ba8cc82296306d4fff93c108c79a70e7
kajisha <h-kajisha@esm.co.jp>
2014-02-15 15:15:05 +0900

    updated

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ln -f gitconfig.local ~/.gitconfig.local
 ln -f vimrc.local ~/.vimrc.local
-ln -f zshrc.local ~/.zshrc.local
+ln -f vimrc.bundles.local ~/.vimrc.bundles.local
+ln -f aliases.local ~/.aliases.local
 ln -f tmux.conf.local ~/.tmux.conf.local
