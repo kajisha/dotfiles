09a59d070e275e5a8610821d1c1407e36ba4aec8
kajisha <kajisha@gmail.com>
2015-06-06 20:11:56 +0900

    use rcm

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ln -f gitconfig.local ~/.gitconfig.local
-ln -f vimrc.local ~/.vimrc.local
-ln -f vimrc.bundles.local ~/.vimrc.bundles.local
-ln -f aliases.local ~/.aliases.local
-ln -f tmux.conf.local ~/.tmux.conf.local
