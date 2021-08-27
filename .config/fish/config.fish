26579fd29b9b8958866f27703f98f93a8db224b4
Hiroshi Kajisha <kajisha@gmail.com>
2021-08-27 12:27:37 +0900

    Update config
    .config/fish/config.fish: Add fzf_key_bindings
    .tool-versions: Update versions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,8 @@ starship init fish | source
 
 eval (asdf exec direnv hook fish)
 
+fzf_key_bindings
+
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 export GPG_TTY=(tty)
 gpg-agent -q --daemon 2> /dev/null
