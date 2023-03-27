86e9e6890496707307bccc3e57dde8ab2dcdbd5e
Hiroshi Kajisha <kajisha@gmail.com>
2023-03-28 05:03:09 +0900

    Add nvim-osc52
    Use ANSI OSC52 sequence to copy text to the system clipboard.
    NOTE: Use Ctrl+V to paste text, Neovim cannot work pasting with *p*

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -14,6 +14,27 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
   {
+    'ojroques/nvim-osc52',
+    config = function()
+      local osc52 = require('osc52')
+      local copy = function(lines, _) osc52.copy(table.concat(lines, '\n')) end
+      local paste = function() return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') } end
+
+      vim.g.clipboard = {
+        name = 'OSC52',
+        copy = {
+          ['+'] = copy,
+          ['*'] = copy,
+        },
+        paste = {
+          ['+'] = paste,
+          ['*'] = paste,
+        },
+      }
+    end
+  },
+
+  {
     'sainnhe/gruvbox-material',
     config = function()
       vim.opt.background = 'dark'
