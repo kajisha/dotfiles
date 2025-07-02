e6037c3190ce890d3747f39748869271fb8a3876
Hiroshi Kajisha <kajisha@gmail.com>
2025-07-02 17:15:10 +0900

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,16 @@
 # fish_default_key_bindings
-
-source ~/.asdf/asdf.fish
-
-starship init fish | source
-
-eval (asdf exec direnv hook fish)
-
 fzf_key_bindings
+
+~/.local/bin/mise activate fish | source
 
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
 export GPG_TTY=(tty)
 gpg-agent -q --daemon 2> /dev/null
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/hiroshi/.ghcup/bin # ghcup-env
+
+# opam configuration
+# source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+~/.local/share/mise/shims/starship init fish | source
+zoxide init fish | source
